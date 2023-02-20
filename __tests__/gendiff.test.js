96fd19f858dd539aed6fd0ccabb8ba38809a9354
animch <nimchuk996@gmail.com>
2023-02-20 23:26:12 +1000

    refactoring test

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -5,42 +5,18 @@ import genDiff from '../src/index.js';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
+const getFixturesPath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
 
-const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
-const readFile = (filename) => readFileSync(getFixturePath(filename), 'utf-8');
+describe.each([
+  ['json'], ['yml'],
+])('$s extension', (extension) => {
+  test.each([
+    ['stylish'], ['plain'], ['json'],
+  ])('%s formatter', (formatter) => {
+    const file1Path = `__fixtures__/file1.${extension}`;
+    const file2Path = `__fixtures__/file2.${extension}`;
+    const expected = readFileSync(getFixturesPath(`${formatter}Result.txt`), 'utf-8');
 
-const filejson1 = getFixturePath('file1.json');
-const filejson2 = getFixturePath('file2.json');
-const fileyaml1 = getFixturePath('file1.yml');
-const fileyaml2 = getFixturePath('file2.yml');
-const expectedResultJson = readFile('resultJson.txt');
-const expectedResultStylish = readFile('resultStylish.txt');
-const expectedResultPlain = readFile('resultPlain.txt');
-test.each([
-  {
-    a: filejson1, b: filejson2, format: 'json', expectresult: expectedResultJson,
-  },
-  {
-    a: fileyaml1, b: fileyaml2, format: 'json', expectresult: expectedResultJson,
-  },
-  {
-    a: fileyaml1, b: fileyaml2, format: 'stylish', expectresult: expectedResultStylish,
-  },
-  {
-    a: filejson1, b: filejson2, format: 'stylish', expectresult: expectedResultStylish,
-  },
-  {
-    a: filejson1, b: filejson2, format: 'plain', expectresult: expectedResultPlain,
-  },
-  {
-    a: fileyaml1, b: fileyaml2, format: 'plain', expectresult: expectedResultPlain,
-  },
-  {
-    a: filejson1, b: filejson2, expectresult: expectedResultStylish,
-  },
-
-])('format', ({
-  a, b, format, expectresult,
-}) => {
-  expect(genDiff(a, b, format)).toBe(expectresult);
+    expect(genDiff(file1Path, file2Path, formatter)).toBe(expected);
+  });
 });
