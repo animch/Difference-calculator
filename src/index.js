86cf1592407c8f0fe83d1441a981be1b697ad6d3
animch <nimchuk996@gmail.com>
2023-02-22 22:56:24 +1000

    refactoring

--- src/index.js
+++ src/index.js
@@ -7,15 +7,9 @@ import buildDiffTree from './diffTree.js';
 const getPath = (filepath) => path.resolve(filepath);
 const readFile = (filepath) => readFileSync((getPath(filepath)), 'utf-8');
 const getFormat = (filepath) => path.extname(filepath).slice(1);
+const parsedData = (filepath) => parseFile(readFile(filepath), getFormat(filepath));
 
 export default ((filepath1, filepath2, format = 'stylish') => {
-  const file1 = readFile(filepath1);
-  const file2 = readFile(filepath2);
-  const format1 = getFormat(filepath1);
-  const format2 = getFormat(filepath2);
-  const parsedFile1 = parseFile(file1, format1);
-  const parsedFile2 = parseFile(file2, format2);
-  const tree = buildDiffTree(parsedFile1, parsedFile2);
-
+  const tree = buildDiffTree(parsedData(filepath1), parsedData(filepath2));
   return formater(tree, format);
 });
